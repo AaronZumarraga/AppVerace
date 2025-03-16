43de29f36066c1001157fb3e2aa861c7638512a8
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2025-03-16 11:56:31 -0500

    Prueba de botones

--- app/(tabs)/cuenta.tsx
+++ app/(tabs)/cuenta.tsx
@@ -1,9 +1,12 @@
 import { View, Text, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
 
 export default function Tab() {
   return (
     <View style={styles.container}>
       <Text>Tab [Cuenta]</Text>
+      <Button theme="primary" label="Choose a photo" />
+      <Button label="Use this photo" />
     </View>
   );
 }
